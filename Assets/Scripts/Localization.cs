ec0b25a4593288e64f8f4f679b2a96f4050ef7bf
Holo <holo@frozent.pl>
2020-09-24 03:43:24 +0200

    Updated player.cs; Seperate strings

---
+++ Assets/Scripts/Localization.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Localization {
+    public static string HOLD_TO_LOOT = "Hold [e] to loot";
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,35 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class player : MonoBehaviour{
+    // #############################################
+    // ##### Structs
+    
     // LootInfo stores information about loot
-    struct LootInfo {
-        string lootName;
-        float lootTime;
-        float lootWorth;
-        int lootWeight;
+    private struct LootInfo {
+        public string name;
+        public float time;
+        public float worth;
+        public int weight;
     }
     
     // #############################################
     // ##### VARIABLES
-    private CharacterController controller;
-    private Vector3 move;
+    
+    [Header("Player stats")]
+    [Tooltip("Base move speed of player")]
     public float moveSpeed = 5f;
+    
+    [Header("Camera")]
+    [Tooltip("Set minimum (X) and maximum (Y) camera zoom levels")]
+    public Vector2 cameraZoomBorders;
+    // Loot information
+    private LootInfo lootInfo;
+    private CharacterController chararacterController;
     public float cameraScrollSpeed = 1f;
     public GameObject canvasObj;
-    public GameObject lootObj;
+    
+    // Current loot object or null if not in zone of any
+    private GameObject currentLootObj;
     public RectTransform informationTextTransform;
     public TMPro.TextMeshProUGUI informationTextObj;
     public TMPro.TextMeshProUGUI infoToHold;
     public gameController gameController;
+    
     private Vector3 offsetText = new Vector3(0, 150, 0);
     private short lootEntered = 0;
     private List<GameObject> lootableObjects = new List<GameObject>();
     private float timerToLoot = 0f;
-    private float toLoot = 0f;
     
     // #############################################
     // ##### METHODS
+    
+    // Update information about loot in LootInfo struct
+    void UpdateLootInfo(lootInformation a_ref){
+        lootInfo.name = a_ref.lootName;
+        lootInfo.time = a_ref.timeToLoot;
+        lootInfo.worth = a_ref.lootScore;
+        lootInfo.weight = a_ref.lootWeight;
+    }
+    
+    // Reset information about loot in LootInfo struct
+    void ResetLootInfo(){
+        lootInfo.name = "";
+        lootInfo.time = 0f;
+        lootInfo.worth = 0f;
+        lootInfo.weight = 0;
+    }
+    
+    // Delete object from lootables
+    void DeleteLootableObj(GameObject a_obj){
+        // Remove object from lootable object list
+        lootableObjects.Remove(a_obj);
+        // Add -1 to lootable objects counter
+        lootEntered--;
+        // If list is empty
+        if(lootEntered == 0){
+            // Reset text in pop-up text
+            informationTextObj.SetText("");
+            // Set current lootable object to null
+            currentLootObj = null;
+            // Reset information about loot
+            ResetLootInfo();
+        // Otherwise
+        } else {
+            // Set current lootable object to last visited
+            currentLootObj = lootableObjects[lootableObjects.Count-1];
+            // Update information about loot in LootInfo struct
+            UpdateLootInfo(currentLootObj.GetComponent<lootInformation>());
+        }
+    }
     
     // #############################################
     // ##### EVENTS
@@ -49,47 +102,50 @@ public class player : MonoBehaviour{
         // If player enters loot trigger
         if(collider.tag.Equals("Loot")){
             // Save last "lootable" object to variable
-            lootObj = collider.gameObject;
+            currentLootObj = collider.gameObject;
             // If given object is not in our lootable object list, add it
-            if(!lootableObjects.Contains(lootObj)){
-                lootableObjects.Add(lootObj);
+            if(!lootableObjects.Contains(currentLootObj)){
+                lootableObjects.Add(currentLootObj);
             }
-            // Save information about loot in 
-            toLoot = lootObj.GetComponent<lootInformation>().timeToLoot;
+            // Update information about loot in LootInfo struct
+            UpdateLootInfo(currentLootObj.GetComponent<lootInformation>());
+            // Add +1 to lootable objects counter
             lootEntered++;
-            if(lootEntered == 1){
-                informationTextObj.SetText("Hold [e] to loot");
+            // If there is at least 1 lootable object, change text of pop-up text
+            if(lootEntered > 0){
+                informationTextObj.SetText(Localization.HOLD_TO_LOOT);
             }
+        //-----------------------------------------
         // Else if player enters loot zone trigger
         } else if(collider.tag.Equals("Loot Zone")){
-            
         }
     }
     
+    // On trigger exit
     void OnTriggerExit(Collider collider){
+        // If player exit loot trigger
         if(collider.tag.Equals("Loot")){
-            lootableObjects.Remove(collider.gameObject);
-            lootEntered--;
-            if(lootEntered == 0){
-                informationTextObj.text = "";
-                lootObj = null;
-                toLoot = 0f;
-            } else {
-                lootObj = lootableObjects[lootableObjects.Count-1].gameObject;
-                toLoot = lootObj.GetComponent<lootInformation>().timeToLoot;
-            }
+            // Delete that object from lootables
+            DeleteLootableObj(collider.gameObject);
         }
     }
     
+    // On start
     void Start(){
-        lootObj = null;
-        controller = GetComponent<CharacterController>();
+        // Init loot information struct
+        lootInfo = new LootInfo();
+        ResetLootInfo();
+        
+        // Set current loot object to null (not in area of any loot object)
+        currentLootObj = null;
+        // Get components and save their references
+        chararacterController = GetComponent<CharacterController>();
     }
 
     // Every frame
     void Update(){
-        // Reset move vector
-        move = Vector3.zero;
+        // Init move vector
+        Vector3 move = Vector3.zero;
         // Check if WSAD is hold and add to move vector
         if(Input.GetKey("w")){
             move.x += 1f;
@@ -107,43 +163,47 @@ public class player : MonoBehaviour{
             move.x += 1f;
             move.z -= 1f;
         }
+        // Normalize move vector
+        move = move.normalized;
+        // Apply small gravity
+        move.y = -5f;
+        // If player is on ground, apply only friction of gravity
+        if(chararacterController.isGrounded){
+            move.y = -0.01f;
+        }
         //----------------------------------
         // If there is any loot to get and player is holding "e" (loot button)
         if(lootEntered > 0 && Input.GetKey("e")){
             // Add to "holding to loot" timer
             timerToLoot += Time.deltaTime;
             // If our timer passed loot time
-            if(timerToLoot >= toLoot){
+            if(timerToLoot >= lootInfo.time){
+                // Reset that timer
                 timerToLoot = 0f;
-                gameController.AddToWorth(lootObj.GetComponent<lootInformation>().lootScore);
-                lootObj.SetActive(false);
-                lootableObjects.Remove(lootObj);
-                lootEntered--;
-                if(lootEntered == 0){
-                    informationTextObj.text = "";
-                    lootObj = null;
-                    toLoot = 0f;
-                } else {
-                    lootObj = lootableObjects[lootableObjects.Count-1].gameObject;
-                    toLoot = lootObj.GetComponent<lootInformation>().timeToLoot;
-                }
+                // Add score
+                gameController.AddToWorth(lootInfo.worth);
+                // Set current lootable object as not active
+                currentLootObj.SetActive(false);
+                // Delete object from lootables
+                DeleteLootableObj(currentLootObj);
             }
+        // Otherwise reset timer
         } else {
             timerToLoot = 0f;
         }
-        infoToHold.SetText(timerToLoot.ToString() + "/" + toLoot.ToString());
-        move = move.normalized;
-        move.y -= 5f;
-        if(controller.isGrounded){
-            move.y = 0.01f;
-        }
+        // [DEBUG] Update timer information
+        infoToHold.SetText(timerToLoot.ToString() + "/" + lootInfo.time.ToString());
+        //----------------------------------
+        // Change zoom of camera with mouse scroll
         if(Input.mouseScrollDelta.y != 0f){
+            // Set camera zoom
             float t_scrollZoom = Camera.main.orthographicSize-Input.mouseScrollDelta.y*cameraScrollSpeed;
-            Camera.main.orthographicSize = Mathf.Clamp(t_scrollZoom, 4f, 10f);
+            Camera.main.orthographicSize = Mathf.Clamp(t_scrollZoom, cameraZoomBorders.x, cameraZoomBorders.y);
         }
-        if(!(lootObj is null)){
-            informationTextTransform.position = Camera.main.WorldToScreenPoint(lootObj.transform.position)+offsetText*5/Camera.main.orthographicSize;
+        //----------------------------------
+        if(!(currentLootObj is null)){
+            informationTextTransform.position = Camera.main.WorldToScreenPoint(currentLootObj.transform.position)+offsetText*5/Camera.main.orthographicSize;
         }
-        controller.Move(move*Time.deltaTime*moveSpeed);
+        chararacterController.Move(move*Time.deltaTime*moveSpeed);
     }
 }
