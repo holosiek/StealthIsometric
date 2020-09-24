dfd7e4cabfa5d00cc168345b77d33c9a708d74ca
Holo <holo@frozent.pl>
2020-09-24 18:32:20 +0200

    PostProcessing switching

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class gameController : MonoBehaviour{
     // #############################################
-    // ##### Structs
+    // ##### STRUCTS
     
     // LootInfo stores information about loot
     public struct LootInfo {
@@ -15,6 +17,15 @@ public class gameController : MonoBehaviour{
         public float worth;
         public int weight;
         public Sprite image;
+    }
+    
+    // #############################################
+    // ##### ENUMS
+    
+    enum PPSettings{
+        Default,
+        Success,
+        Failed
     }
     
     // #############################################
@@ -34,8 +45,18 @@ public class gameController : MonoBehaviour{
     public TMPro.TextMeshProUGUI textMeshWorthScore;
     [Tooltip("Text mesh of equipped item")]
     public TMPro.TextMeshProUGUI textMeshEquippedItem;
+    [Tooltip("Text mesh of header (like \"Succesful mission\" text)")]
+    public TMPro.TextMeshProUGUI textMeshHeader;
+    [Tooltip("RectTransform of header (like \"Succesful mission\" text)")]
+    public RectTransform textMeshHeaderTransform;
     [Tooltip("Image component of currently equipped item")]
     public Image imageEquippedItem;
+    [Tooltip("Global postprocess volume reference")]
+    public Volume postProcessVolume;
+    // Reference to vignette of post process volume
+    private Vignette postProcessVignette;
+    // Reference to color adjustments of post process volume
+    private ColorAdjustments postProcessColorAdjustments;
     
     [Header("References to objects")]
     [Tooltip("Escape Zone reference, it will appear after objective being completed")]
@@ -51,6 +72,41 @@ public class gameController : MonoBehaviour{
     
     // Loot information struct
     public LootInfo lootInfo;
+    
+    //------------------------
+    // >>> Post processing
+    //------------------------
+    // Normal post processing settings
+    [Header("PP settings - Default")]
+    [Space(20)]
+    [Range(0f, 1f)]
+    [Tooltip("Vignette intensivity")]
+    public float vignetteIntensivityDefault;
+    [Tooltip("ColorAdjustments color (leave white to not change colors)")]
+    public Color colorAdjustmentsColorDefault;
+    
+    // Success mission post processing settings
+    [Header("PP settings - Successful Mission")]
+    [Range(0f, 1f)]
+    [Tooltip("Vignette intensivity")]
+    public float vignetteIntensivitySuccess;
+    [Tooltip("ColorAdjustments color (leave white to not change colors)")]
+    public Color colorAdjustmentsColorSuccess;
+    
+    // Failed mission post processing settings
+    [Header("PP settings - Failed Mission")]
+    [Range(0f, 1f)]
+    [Tooltip("Vignette intensivity")]
+    public float vignetteIntensivityFailed;
+    [Tooltip("ColorAdjustments color (leave white to not change colors)")]
+    public Color colorAdjustmentsColorFailed;
+    
+    // Is post processing in progress of updating
+    bool isPPUpdating = false;
+    // Which PP setting is set
+    PPSettings whichPPSettingisSet = PPSettings.Default;
+    // Post processing timer for interp values
+    float ppInterpTimer = 0f;
     
     // #############################################
     // ##### METHODS
@@ -90,6 +146,31 @@ public class gameController : MonoBehaviour{
         textMeshWorthScore.SetText(currentWorth.ToString("0.000") + "$ / " + totalWorth.ToString("0.000") + "$");
     }
     
+    // Update post processing settings
+    private void UpdatePP(PPSettings a_settings){
+        // Switch settings
+        switch(a_settings){
+            // Set to default, don't interpolate
+            case PPSettings.Default: {
+                postProcessVignette.intensity.value = vignetteIntensivityDefault;
+                postProcessColorAdjustments.colorFilter.value = colorAdjustmentsColorDefault;
+                break;
+            }
+            // Set to successful PP, interpolate values
+            case PPSettings.Success: {
+                postProcessVignette.intensity.Interp(vignetteIntensivityDefault, vignetteIntensivitySuccess, ppInterpTimer);
+                postProcessColorAdjustments.colorFilter.Interp(colorAdjustmentsColorDefault, colorAdjustmentsColorSuccess, ppInterpTimer);
+                break;
+            }
+            // Set to failed PP, interpolate values
+            case PPSettings.Failed: {
+                postProcessVignette.intensity.Interp(vignetteIntensivityDefault, vignetteIntensivityFailed, ppInterpTimer);
+                postProcessColorAdjustments.colorFilter.Interp(colorAdjustmentsColorDefault, colorAdjustmentsColorFailed, ppInterpTimer);
+                break;
+            }
+        }
+    }
+    
     //------------------------
     // >>> Loot
     //------------------------
@@ -125,6 +206,45 @@ public class gameController : MonoBehaviour{
         ResetLootInfo();
         
         // Update UI
+        UpdateWorth();
         UpdateEquipped();
+        
+        // Save references to post process volume settings
+        postProcessVolume.profile.TryGet<Vignette>(out postProcessVignette);
+        postProcessVolume.profile.TryGet<ColorAdjustments>(out postProcessColorAdjustments);
+        
+        // Set post process settings to default
+        UpdatePP(PPSettings.Default);
+    }
+    
+    // Every frame
+    void Update(){
+        // [DEBUG] change pp settings
+        if(Input.GetKeyDown("v")){
+            isPPUpdating = true;
+            whichPPSettingisSet = PPSettings.Success;
+        }
+        if(Input.GetKeyDown("b")){
+            isPPUpdating = true;
+            whichPPSettingisSet = PPSettings.Failed;
+        }
+        if(Input.GetKeyDown("c")){
+            UpdatePP(PPSettings.Default);
+        }
+        
+        // If PP is updating, interp values of it
+        if(isPPUpdating){
+            UpdatePP(whichPPSettingisSet);
+            // If we are interpolating values
+            if(whichPPSettingisSet != PPSettings.Default){
+                // Add to interpolation timer
+                ppInterpTimer += Time.deltaTime;
+            }
+            // If interpolation timer is over 1.0, reset it and we finished updating PP
+            if(ppInterpTimer >= 1.0f){
+                isPPUpdating = false;
+                ppInterpTimer = 0f;
+            }
+        }
     }
 }
