f76d1049db8f47efb5322cf05e59b29ff1337168
Holo <holo@frozent.pl>
2020-09-29 04:00:26 +0200

    Added AudioManager and rain sounds

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour{
+    /*
+        Based on Brackeys AudioManager
+    */
+    
+    // #############################################
+    // ##### VARIABLES
+    
+    [Tooltip("Instance of AudioManager")]
+	public static AudioManager instance;
+    
+    [Tooltip("Array containing sounds to play")]
+	public Sound[] sounds;
+
+    // #############################################
+    // ##### METHODS
+    
+    // Play sound
+	public void Play(string a_sound){
+        // Find sound in array
+		Sound sound = Array.Find(sounds, item => item.name == a_sound);
+        // If sound doesn't exist, throw warning and return function
+		if(sound == null){
+			Debug.LogWarning("Sound: " + a_sound + " not found!");
+			return;
+		}
+        // Play found sound
+		sound.source.Play();
+    }
+    
+    // #############################################
+    // ##### EVENTS
+    
+	void Awake(){
+        // Check if AudioManager already exists, if yes, destroy this component, if not, set this as main instance;
+        // Also don't destroy it on load another scene
+		if(instance != null){
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+        
+        // For each sound in array
+		foreach(Sound sound in sounds){
+            // Create AudioSource component
+			sound.source = gameObject.AddComponent<AudioSource>();
+            // Apply sound settings to AudioSource
+			sound.source.clip = sound.clip;
+            sound.source.loop = sound.loop;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            // If autoplay is on, play sound
+            if(sound.autoplay){
+                sound.source.Play();
+            }
+		}
+	}
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[Serializable]
+public class Sound{
+    // #############################################
+    // ##### VARIABLES
+    
+    [Tooltip("Name of sound")]
+	public string name;
+    
+    [Tooltip("Audio clip source")]
+	public AudioClip clip;
+
+	[Range(0f, 1f)]
+    [Tooltip("Volume of audio")]
+	public float volume = 1f;
+
+	[Range(0.01f, 3f)]
+    [Tooltip("Pitch of audio")]
+	public float pitch = 0f;
+
+    [Tooltip("Should audio be looped?")]
+	public bool loop = false;
+    
+    [Tooltip("Should be autoplayed?")]
+	public bool autoplay = false;
+
+    // AudioSource component
+	[HideInInspector]
+	public AudioSource source;
+}
